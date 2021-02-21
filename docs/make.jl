4466cc2c53f66e5fe7e56db7929f8ef26a140083
Varnerlab <jdv27@cornell.edu>
2021-02-21 11:30:16 -0500

    Update make.jl
    gh-pages

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,12 @@
 using Documenter, PregnancyCoagDataAnalysisKit
 
 makedocs(sitename="Documentation")
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+deploydocs(
+    repo = "github.com/varnerlab/PregnancyCoagDataAnalysisKit.jl.git",
+    devbranch = "main",
+    devurl = "dev",
+)
